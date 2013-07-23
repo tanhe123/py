355fea309b8304a540f395c4c324ae3ee4c245c1
tanhehe <443016215@qq.com>
2013-07-23 10:29:28 +0800

    update GetImgFromPage

--- GetImgFromPage.py
+++ GetImgFromPage.py
@@ -10,15 +10,15 @@ def getHtml(url):
     print "读取网页成功...."
     return html
 
-def getImg(url):
+def getImg(url,n):
     html = getHtml(url);
     reg = r'src="(http://.{0,300}?(?:\.jpg|\.gif|\.jpeg|\.png))"'
     imgre = re.compile(reg)
     print "正则表达式编译成功..."
     imglist = imgre.findall(html)
     print "正在查找网页中的图片..."
-    x = 0
-    local = '/home/tan/code/py/down/'
+    x = n
+    local = '/home/tan/code/'
     for imgurl in imglist:
         print '正在下载',imgurl
         if imgurl[-2] == 'p':
@@ -30,12 +30,15 @@ def getImg(url):
         elif imgurl[-2] == 'n':
             urllib.urlretrieve(imgurl, local+'%s.png' % x)
         x += 1
+        
+html = raw_input("请输入想要下载的网址:\n")
+n = int(raw_input("输入n:"))
 
-getImg('http://www.baidu.com')
+if html.find("http:") == -1:
+    html = "http://" + html
 
 
 
+print '准备获取'+html+'中的图片'
 
-
-
-
+getImg(html, n)
