cb8f0b8423f4e2d4fdde77726ce093ea65201e04
tanhehe <443016215@qq.com>
2013-07-23 10:36:18 +0800

    update GetImgFromPage

--- GetImgFromPage.py
+++ GetImgFromPage.py
@@ -10,29 +10,27 @@ def getHtml(url):
     print "读取网页成功...."
     return html
 
-def getImg(url,n):
+def getImg(url):
     html = getHtml(url);
     reg = r'src="(http://.{0,300}?(?:\.jpg|\.gif|\.jpeg|\.png))"'
     imgre = re.compile(reg)
     print "正则表达式编译成功..."
     imglist = imgre.findall(html)
     print "正在查找网页中的图片..."
-    x = n
-    local = '/home/tan/code/'
+    x = 0
     for imgurl in imglist:
         print '正在下载',imgurl
         if imgurl[-2] == 'p':
-            urllib.urlretrieve(imgurl, local+'%s.jpg' % x)
+            urllib.urlretrieve(imgurl, '%s.jpg' % x)
         elif imgurl[-2] == 'i':
-            urllib.urlretrieve(imgurl, local+'%s.gif' % x)
+            urllib.urlretrieve(imgurl, '%s.gif' % x)
         elif imgurl[-2] == 'e':
-            urllib.urlretrieve(imgurl, local+'%s.jpeg' % x)
+            urllib.urlretrieve(imgurl, '%s.jpeg' % x)
         elif imgurl[-2] == 'n':
-            urllib.urlretrieve(imgurl, local+'%s.png' % x)
+            urllib.urlretrieve(imgurl, '%s.png' % x)
         x += 1
         
 html = raw_input("请输入想要下载的网址:\n")
-n = int(raw_input("输入n:"))
 
 if html.find("http:") == -1:
     html = "http://" + html
@@ -41,4 +39,4 @@ if html.find("http:") == -1:
 
 print '准备获取'+html+'中的图片'
 
-getImg(html, n)
+getImg(html)
